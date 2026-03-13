10da2572937fc2b9324a9e8e478d6f772f3b8ab8
Tien Kim <tienkim9920@gmail.com>
2026-03-13 16:28:15 +0700

    update auth controller

--- src/main/java/com/basicspringboot/ninedev/controllers/AuthController.java
+++ src/main/java/com/basicspringboot/ninedev/controllers/AuthController.java
@@ -1,10 +1,12 @@
 package com.basicspringboot.ninedev.controllers;
 
 import com.basicspringboot.ninedev.dto.LoginRequest;
+import com.basicspringboot.ninedev.dto.ResponseDTO;
 import com.basicspringboot.ninedev.entites.UserEntity;
 import com.basicspringboot.ninedev.repositories.UserRepository;
 import com.basicspringboot.ninedev.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -18,7 +20,7 @@ public class AuthController {
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
-    public Map<String, String> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequest request) {
 
         UserEntity user = userRepository
                 .findByUsername(request.getUsername())
@@ -30,6 +32,8 @@ public class AuthController {
 
         String token = jwtUtil.generateToken(user.getUsername());
 
-        return Map.of("token", token);
+        return ResponseEntity.ok(
+                new ResponseDTO(200, true, "Login Success", token)
+        );
     }
 }
